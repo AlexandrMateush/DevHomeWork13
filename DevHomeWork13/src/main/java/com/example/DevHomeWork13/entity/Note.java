b1acc2ac73d7741242cd3a77eb39fb31e80166bc
AlexandrMateush <128997943+AlexandrMateush@users.noreply.github.com>
2023-09-04 16:14:48 +0300

    Add files via upload

--- DevHomeWork13/src/main/java/com/example/DevHomeWork13/entity/Note.java
+++ DevHomeWork13/src/main/java/com/example/DevHomeWork13/entity/Note.java
@@ -1,6 +1,14 @@
 package com.example.DevHomeWork13.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String content;
